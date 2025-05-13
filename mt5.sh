02fad6415f3d7e2791a63f3f9a2ab809772d6594
Gabriel <bielp07@gmail.com>
2025-05-13 13:19:39 -0300

    Adiciona instruções pra execução de programas em python que integram mt5 via wine

--- mt5.sh
+++ mt5.sh
@@ -1,1 +1,4 @@
 WINEPREFIX=~/.mt5 wine ~/.mt5/drive_c/Program\ Files/MetaTrader\ 5/terminal64.exe
+
+# Executar programas que utilizam a biblioteca MetaTrader5 (Python 3.8.9 32bits):
+# WINEPREFIX=~/.mt5 wine py bgfxh.py
